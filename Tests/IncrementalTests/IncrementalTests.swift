79216bd5f69c958b6d1edea4bccb1b50def054d2
Sasha Lopoukhine <superlopuh@gmail.com>
2017-11-02 11:56:52 +0100

    add retention cycle test

--- Tests/IncrementalTests/IncrementalTests.swift
+++ Tests/IncrementalTests/IncrementalTests.swift
@@ -11,13 +11,53 @@ import XCTest
 import Incremental
 
 class IncrementalTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(Incremental().text, "Hello, World!")
+
+    // Tests that once a node is unreachable, it is deallocated
+    func testMemoryCycle() {
+        var count = 0
+
+        func counter() {
+            count += 1
+        }
+
+        let input = Input(())
+        var node: Node<()>? = Incremental.map(input.node, counter)
+
+        // `counter` fired on creation
+        XCTAssertEqual(1, count)
+
+        input.value = ()
+
+        // `counter` fired on update
+        XCTAssertEqual(2, count)
+
+        var _count = 0
+        // Captures node
+        var observer: Observer<()>? = node?.observe { _count += 1 }
+        touch(observer)
+
+        // Old result node now unreachable
+        node = nil
+
+        input.value = ()
+
+        // Even though node is unreachable, it is captured by observer
+        XCTAssertEqual(3, count)
+
+        // Observer should mutate `_count`
+        XCTAssertEqual(1, _count)
+
+        observer = nil
+
+        input.value = ()
+
+        // `counter` no longer fired on update
+        XCTAssertEqual(3, count)
+        // `observer` no longer fired on update
+        XCTAssertEqual(1, _count)
     }
     
     static var allTests = [
-        ("testExample", testExample),
+        ("testMemoryCycle", testMemoryCycle),
     ]
 }
